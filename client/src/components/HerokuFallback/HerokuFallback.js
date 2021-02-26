48abe07bbeb6b9aa9f03d4e24f244284d22558cf
Nikolas Theodoropoulos <nikolastheodoropoulos@Nikolass-MacBook-Air.local>
2021-02-26 13:21:19 +0200

    added heroku fallback

--- client/src/components/HerokuFallback/HerokuFallback.js
+++ client/src/components/HerokuFallback/HerokuFallback.js
@@ -5,7 +5,8 @@ const HerokuFallback = () => {
     return (
         <main className="heroku-page-container">
             <div className="heroku-loading-spin">
-               heroku is loading
+                <h1 className="heroku-fallback-msg">Please wait until heroku warms up...</h1>
+                <div className="lds-circle"><div></div></div>
             </div>
         </main>
     );
